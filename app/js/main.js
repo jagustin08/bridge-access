ea6c5c8fd489ac9964d97da6bb70b9fc0d12b567
Jayson Agustin <dev.jagustin@gmail.com>
2022-02-13 03:29:57 +0800

    Add nav-toggle animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // preloader animation
-const logoStrokes = document.querySelectorAll(".preloader-logo path, .preloader-logo rect");
+/* const logoStrokes = document.querySelectorAll(".preloader-logo path, .preloader-logo rect");
 
 logoStrokes.forEach(logoStroke => {
 
@@ -25,4 +25,40 @@ logoStrokes.forEach(logoStroke => {
         height: '0%',
         ease: Power2.easeOut
     }, '-=10%')    
+});
+ */
+
+// nav toggle animation
+const navToggle = document.querySelector(".nav-toggle");
+const toggleLines = document.querySelector(".toggle-lines");
+const lineOne = toggleLines.querySelector("rect:nth-child(1)");
+const lineTwo = toggleLines.querySelector("rect:nth-child(2)");
+const lineThree = toggleLines.querySelector("rect:nth-child(3)");
+
+const toggleAnimation = gsap.timeline({
+  reversed: true,
+  paused: true,
+});
+
+gsap.set([lineOne, lineTwo, lineThree], { transformOrigin: "50% 50%" });
+
+toggleAnimation
+  .to(lineOne, 0.35, { 
+      y: 14, 
+    })
+  .to( lineThree, 0.35, {
+      y: -14,
+    }, "<" )
+  .to( lineTwo, 0.35, {
+      scaleX: 0,
+    }, "-=50%" )
+  .to(lineOne, 0.35, {
+    rotate: 45,
+  })
+  .to( lineThree, 0.35, {
+      rotate: -45,
+    }, "<" );
+
+navToggle.addEventListener("click", () => {
+  toggleAnimation.reversed() ? toggleAnimation.play() : toggleAnimation.reverse();
 });
