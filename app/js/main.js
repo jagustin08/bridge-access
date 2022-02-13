fa0c781928d91499bc869d35dff6fab428764d7a
Jayson Agustin <dev.jagustin@gmail.com>
2022-02-13 19:44:06 +0800

    Add hero animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,13 @@
 "use strict";
 
 // preloader animation
-/* const logoStrokes = document.querySelectorAll(".preloader-logo path, .preloader-logo rect");
+const logoStrokes = document.querySelectorAll(".preloader-logo path, .preloader-logo rect");
 
 logoStrokes.forEach(logoStroke => {
 
     const preloaderTimeline = gsap.timeline();
 
-    preloaderTimeline.fromTo(logoStroke, 1.5, {
+    preloaderTimeline.fromTo(logoStroke, 2, {
         strokeDashoffset: logoStroke.getTotalLength(),
         strokeDasharray: logoStroke.getTotalLength(),
     }, {
@@ -23,10 +23,31 @@ logoStrokes.forEach(logoStroke => {
     })    
     .to('.preloader', 1, {
         height: '0%',
-        ease: Power2.easeOut
-    }, '-=10%')    
+        ease: Power2.easeOut,
+    }, '-=10%')
+    .fromTo('.hero-img', 0.4, {
+      scale: 1.1,
+      ease: Power2.easeOut,
+      filter: 'blur(1px)'
+    }, {
+      scale: 1,
+      filter: 'blur(0px)'
+    }, '-=90%')
+    .fromTo('.main-header', 0.4, {
+      yPercent: -100,
+    }, {
+      yPercent: 0,
+      ease: Power2.easeOut,
+      delay: 0.4,      
+    }, '-=40%')    
+    .to('.hero .fadeInUp', 0.4, {
+      opacity: 1,
+      y: 0,
+      stagger: 0.15,
+      ease: Power2.easeOut
+    })
 });
- */
+
 
 // nav toggle animation
 const navToggle = document.querySelector(".nav-toggle");
@@ -35,30 +56,50 @@ const lineOne = toggleLines.querySelector("rect:nth-child(1)");
 const lineTwo = toggleLines.querySelector("rect:nth-child(2)");
 const lineThree = toggleLines.querySelector("rect:nth-child(3)");
 
+gsap.set([lineOne, lineTwo, lineThree], { transformOrigin: "50% 50%" });
+
 const toggleAnimation = gsap.timeline({
   reversed: true,
   paused: true,
 });
 
-gsap.set([lineOne, lineTwo, lineThree], { transformOrigin: "50% 50%" });
-
 toggleAnimation
-  .to(lineOne, 0.35, { 
-      y: 14, 
-    })
-  .to( lineThree, 0.35, {
+  .to(lineOne, 0.15, {
+    y: 14,
+  })
+  .to( lineThree, 0.15, {
       y: -14,
     }, "<" )
-  .to( lineTwo, 0.35, {
+  .to( lineTwo, 0.15, {
       scaleX: 0,
     }, "-=50%" )
-  .to(lineOne, 0.35, {
-    rotate: 45,
-  })
-  .to( lineThree, 0.35, {
-      rotate: -45,
-    }, "<" );
+  .to(lineOne, 0.15, {
+    rotate: 45, })
+  .to( lineThree, 0.15, { 
+    rotate: -45,
+    }, "<" )
+  .fromTo( ".main-nav", 0.35, {
+      height: 0,
+      ease: Power2.easeInOut,
+    }, {
+      height: "auto",
+    }, "<" )
 
 navToggle.addEventListener("click", () => {
   toggleAnimation.reversed() ? toggleAnimation.play() : toggleAnimation.reverse();
+});
+
+// Initialize swiper
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
